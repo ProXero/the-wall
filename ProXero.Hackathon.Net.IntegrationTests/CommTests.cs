0b1f9577df6497b9ee698efd122ad6d3439b569c
Csabi <b.csabor@gmail.com>
2015-12-23 22:08:52 +0100

    fix test

--- ProXero.Hackathon.Net.IntegrationTests/CommTests.cs
+++ ProXero.Hackathon.Net.IntegrationTests/CommTests.cs
@@ -16,13 +16,18 @@ namespace ProXero.Hackathon.Net.IntegrationTests
 		public void test1()
 		{
 			Server<MessageReceiver> s = new Server<MessageReceiver>();
+			AutoResetEvent receive = new AutoResetEvent(false);
 			Client<int> c = new Client<int>();
 			int result = 0;
 
-			c.Inbox.Subscribe(m => result = m);
+			c.Inbox.Subscribe(m =>
+			{
+				result = m;
+				receive.Set();
+			});
 			c.SendMessage(33);
-			
 
+			receive.WaitOne();
 			s.Close();
 			c.Close();
 
